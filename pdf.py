2fc2c63dc853ab748d63de6a683655adc9fdb535
P10R10 <asantiago@keemail.me>
2023-06-04 19:24:54 +0100

    pdf

--- pdf.py
+++ pdf.py
@@ -5,19 +5,19 @@ def merge_files(a: str, b: str):
     try:
         doc_a = fitz.open(a)  # open the 1st document
     except:
-        print(f"Erro na abertura do ficheiro {a}")
+        print(f"Error opening file {a}")
     try:
         doc_b = fitz.open(b)  # open the 2nd document
-        print("DEB")
         doc_a.insert_pdf(doc_b)  # merge the docs
         doc_a.save("merged.pdf")  # save the merged document
     except:
-        print(f"Erro na abertura do ficheiro {b}")
+        print(f"Error opening file {b}")
+    print("Files merged.")
 
 
 def main():
-    doc_a = input("Qual o nome do primeiro ficheiro? ")
-    doc_b = input("Qual o nome do segundo ficheiro? ")
+    doc_a = input("1st file? ")
+    doc_b = input("2nd file? ")
     merge_files(doc_a, doc_b)
 
 
