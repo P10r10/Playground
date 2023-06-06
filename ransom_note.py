80e0eca1c5445c872a7c9bde516acd8eefd70416
P10r10 <asantiago@keemail.me>
2023-06-06 12:14:32 +0100

    ransom note

--- ransom_note.py
+++ ransom_note.py
@@ -1,12 +1,22 @@
-def canConstruct(ransomNote: str, magazine: str) -> bool:
+def canConstruct2(ransomNote: str, magazine: str) -> bool:
     for letter in ransomNote:
         if letter not in magazine:
             return False
-        else:
-            magazine = magazine.replace(letter, "", 1)
+        magazine = magazine.replace(letter, "", 1)
     return True
 
+def canConstruct(ransomNote: str, magazine: str) -> bool:
+    if len(magazine) < len(ransomNote):
+        return False
+    letters = {}
+    for letter in magazine:
+        if not letters[letter]:
+            letters[letter] = 1
+    print(letters)
+    return True
 
-print(canConstruct("a", "b"))
-print(canConstruct("aa", "ab"))
-print(canConstruct("aa", "aab"))
+print(canConstruct("a", "b"))  # False
+print(canConstruct("aa", "ab"))  # False
+print(canConstruct("aa", "aab"))  # True
+print(canConstruct("paymenow", "aemnopyy"))  # False
+print(canConstruct("paymenow", "aemnopyyw"))  # True
