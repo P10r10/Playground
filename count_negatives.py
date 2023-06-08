f8938b8b384a997c44c4e5c484abd8234d1e350f
P10R10 <asantiago@keemail.me>
2023-06-08 09:51:36 +0100

    count negatives

---
+++ count_negatives.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countNegatives(self, grid: list[list[int]]) -> int:
+        count = 0
+        for row in grid:
+            for n in row:
+                if n < 0:
+                    count += 1
+        return count
+
+
+grid = [[4, 3, 2, -1], [3, 2, 1, -1], [1, 1, -1, -2], [-1, -1, -2, -3]]
+print(Solution().countNegatives(grid))
