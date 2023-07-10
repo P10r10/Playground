04a57a8acd3f16150ebfa2378c353c60b0fd6e7e
P10r10 <asantiago@keemail.me>
2023-07-10 12:25:09 +0100

    pdf

--- pdf.py
+++ pdf.py
@@ -1,7 +1,7 @@
 import fitz
 
 
-def merge_files(a: str, b: str):
+def merge_files(a: str, b: str) -> None:
     try:
         doc_a = fitz.open(a)  # open the 1st document
     except:
@@ -15,10 +15,27 @@ def merge_files(a: str, b: str):
         print(f"Error opening file {b}")
 
 
+def delete_pages(file_name: str, page_nb: int) -> None:
+    try:
+        doc = fitz.open(file_name)  # open the document
+    except:
+        print(f"Error opening file {file_name}")
+    try:
+        doc.delete_page(page_nb - 1)
+        doc.save("deleted.pdf")  # save the deleted page document
+        print("Page deleted.")
+    except:
+        print(f"Error deleting page {page_nb}")
+
+
 def main():
     doc_a = input("1st file? ")
     doc_b = input("2nd file? ")
     merge_files(doc_a, doc_b)
+
+    # doc = input("File name? ")
+    # page_nb = int(input("Page number? "))
+    # delete_pages(doc, page_nb)
 
 
 main()
